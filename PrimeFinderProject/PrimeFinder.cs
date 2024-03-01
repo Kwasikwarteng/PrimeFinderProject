4ab9743018d84ec7d69ae726eec7410d8a84bf20
kwasikwarteng <megakwasikwarteng@gmail.com>
2024-03-01 06:41:59 +0000

    New Prime Algorithm implemented

--- PrimeFinderProject/PrimeFinder.cs
+++ PrimeFinderProject/PrimeFinder.cs
@@ -45,54 +45,89 @@ namespace PrimeFinderProject
 
         private void ShowPrimes(int number, ListBox listBox)
         {
-            List<int> primes = FindPrimes(number);
+            List<int> primes = primeAlgorithm(number);
             UpdateListBox(listBox, primes);
         }
 
-        //Render List of Primes
-        private List<int> FindPrimes(int maxNumber)
+        //Sieve of Eratosthenes Algorithm for Primes
+        private List<int> primeAlgorithm(int maxNumber)
         {
-            List<int> primes = new List<int>();
+            //Array to store primes
+            bool[] isPrime = new bool[maxNumber + 1];
             for (int i = 2; i <= maxNumber; i++)
             {
-                if (IsPrime(i))
+                isPrime[i] = true;
+            }
+
+            //Algorithm implementation
+            for (int j = 2; j * j <= maxNumber; j++)
+            {
+                if (isPrime[j] == true)
                 {
-                    primes.Add(i);
-                }
-            }
-            return primes;
-        }
-
-        //Check For Primes
-        private bool IsPrime(int number)
-        {
-            if (number <= 1)
-            {
-                return false;
-            }
-            if (number == 2)
-            {
-                return true;
-            }
-            if (number % 2 == 0)
-            {
-                return false;
-            }
-
-            //Prime Boundary for algorithm
-            int primeBoundary = (int)Math.Floor(Math.Sqrt(number));
-
-            //Iterate to get primes
-            for (int i = 3; i <= primeBoundary; i += 2)
-            {
-                if (number % i == 0)
-                {
-                    return false;
+                    //Set all multiples of j as not prime
+                    for (int k = j * j; k <= maxNumber; k += j)
+                    {
+                        isPrime[k] = false;
+                    }
                 }
             }
 
-            return true;
+            //Store Primes into a List
+            List<int> primeNumbers = new List<int>();
+            for (int i = 2; i <= maxNumber; i++)
+            {
+                if (isPrime[i])
+                {
+                    primeNumbers.Add(i);
+                }
+            }
+            return primeNumbers;
         }
+
+        ////Render List of Primes
+        //private List<int> FindPrimes(int maxNumber)
+        //{
+        //    List<int> primes = new List<int>();
+        //    for (int i = 2; i <= maxNumber; i++)
+        //    {
+        //        if (IsPrime(i))
+        //        {
+        //            primes.Add(i);
+        //        }
+        //    }
+        //    return primes;
+        //}
+
+        ////Check For Primes
+        //private bool IsPrime(int number)
+        //{
+        //    if (number <= 1)
+        //    {
+        //        return false;
+        //    }
+        //    if (number == 2)
+        //    {
+        //        return true;
+        //    }
+        //    if (number % 2 == 0)
+        //    {
+        //        return false;
+        //    }
+
+        //    //Prime Boundary for algorithm
+        //    int primeBoundary = (int)Math.Floor(Math.Sqrt(number));
+
+        //    //Iterate to get primes
+        //    for (int i = 3; i <= primeBoundary; i += 2)
+        //    {
+        //        if (number % i == 0)
+        //        {
+        //            return false;
+        //        }
+        //    }
+
+        //    return true;
+        //}
 
         //Render Data in list box
         private void UpdateListBox(ListBox listBox, List<int> primes)
